9cf2455a347b9d26d2060aa0410a19e8c08fdee4
arthurchang09 <arthurchang09@gmail.com>
2020-12-19 20:30:53 +0800

    update multiple machine

--- app.py
+++ app.py
@@ -428,8 +428,8 @@ def webhook_handler():
             continue
         if not isinstance(event.message.text, str):
             continue
-        print(f"\nFSM STATE: {machine.state}")
-        print(f"REQUEST BODY: \n{body}")
+        #print(f"\nFSM STATE: {machine.state}")
+        #print(f"REQUEST BODY: \n{body}")
         response = machine[event.source.user_id].advance(event)
         if response == False:
             send_text_message(event.reply_token, "Not Entering any State")
