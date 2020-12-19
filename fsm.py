dcfec0225fce1b881e45f5178a21e24bab22fe59
arthurchang09 <arthurchang09@gmail.com>
2020-12-19 16:15:57 +0800

    update guess

--- fsm.py
+++ fsm.py
@@ -35,7 +35,7 @@ class TocMachine(GraphMachine):
         #return int(text)>self.num
         try:
             int(text)
-            return text>self.num
+            return int(text)>self.num
         except ValueError:
             return False
     def is_going_to_wrong_small(self, event):
@@ -45,7 +45,7 @@ class TocMachine(GraphMachine):
         #return int(text)<self.num
         try:
             int(text)
-            return text<self.num
+            return int(text)<self.num
         except ValueError:
             return False
     def is_going_to_riddle(self, event):
