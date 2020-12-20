1514058ed7e6fac3d4f633851ac18ffe543cfcc8
arthurchang09 <arthurchang09@gmail.com>
2020-12-20 20:26:30 +0800

    modify music and add music database

--- fsm.py
+++ fsm.py
@@ -133,8 +133,9 @@ class TocMachine(GraphMachine):
         for i in range(0,len(music.music_name)):
             #music_list.append(str(i+1)+music.music_name[i]+"\n")
             music_list+=str(i+1)+"."+music.music_name[i]+"\n"
+        push_message(event.source.user_id,music_list)
         reply_token = event.reply_token
-        send_text_message(reply_token, music_list+"選歌請輸入歌曲編號\n"+"隨機播放 請輸入「隨機」\n"+"輸入menu回到主選單")
+        send_text_message(reply_token,"選歌請輸入歌曲編號\n"+"隨機播放 請輸入「隨機」\n"+"輸入menu回到主選單")
         #self.go_back()
     def on_enter_play(self, event):
         print("I'm entering paly")
