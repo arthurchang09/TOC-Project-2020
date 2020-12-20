d9c11abbb1766076504b11c4e3b942a2671c7a09
arthurchang09 <arthurchang09@gmail.com>
2020-12-20 20:13:22 +0800

    modify music and add music database

--- fsm.py
+++ fsm.py
@@ -128,13 +128,14 @@ class TocMachine(GraphMachine):
 
     def on_enter_music(self, event):
         print("I'm entering music")
-        music_list=""
+        #music_list=""
+        push_message(event.source.user_id,"曲目如下：\n")
         for i in range(0,len(music.music_name)-1):
             #music_list.append(str(i+1)+music.music_name[i]+"\n")
-            #push_message(event.source.user_id,str(i+1)+music.music_name[i]+"\n")
-            music_list+=str(i+1)+"."+music.music_name[i]+"\n"
+            push_message(event.source.user_id,str(i+1)+"."+music.music_name[i]+"\n")
+            #music_list+=str(i+1)+"."+music.music_name[i]+"\n"
         reply_token = event.reply_token
-        send_text_message(reply_token, "曲單:\n"+music_list+"選歌請輸入歌曲編號\n"+"隨機播放 請輸入「隨機」\n"+"輸入menu回到主選單")
+        send_text_message(reply_token, "選歌請輸入歌曲編號\n"+"隨機播放 請輸入「隨機」\n"+"輸入menu回到主選單")
         #self.go_back()
     def on_enter_play(self, event):
         print("I'm entering paly")
